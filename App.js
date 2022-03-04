2116566d022c63cdd49ece39dc3f2985d40936ed
Rookie github <85553325+RookieLim@users.noreply.github.com>
2022-03-04 18:47:16 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert, Platform } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./colors";
@@ -20,7 +20,9 @@ export default function App() {
   };
   const loadToDos =async() => {
     const s = await AsyncStorage.getItem(STORAGE_KEY);
-    setToDos(JSON.parse(s));
+    if(s){
+      setToDos(JSON.parse(s));
+    }   
   };
 
    const addToDo = async()=>{
@@ -37,30 +39,40 @@ export default function App() {
     setText("");
   };
   const deleteToDo = (key) => {
-    Alert.alert(
-      "Delete To Do",
-      "Are You sure?", [
-      {text: "Cancel"},
-      {text: "I'm Sure", 
-      onPress: async()=> {
-      const newToDos = {...toDos};
-      delete newToDos[key];
-      setToDos(newToDos);
-      await saveToDos(newToDos);  
+    if(Platform.OS === "web") {
+      const ok=confirm("Do you want to delete this To Do?");
+      if(ok){
+        const newToDos = {...toDos};
+        delete newToDos[key];
+        setToDos(newToDos);
+        saveToDos(newToDos);
+      }
+    }else {Alert.alert(
+        "Delete To Do",
+        "Are You sure?", [
+        {text: "Cancel"},
+        {text: "I'm Sure", 
+        onPress: async()=> {
+        const newToDos = {...toDos};
+        delete newToDos[key];
+        setToDos(newToDos);
+        await saveToDos(newToDos);  
       },
-    },
+    },     
   ]);
-    return;
-  };
+}
+   };
   return (
     <View style={styles.container}>
      <StatusBar style="auto" />
       <View style={styles.header}>
        <TouchableOpacity onPress={work}> 
-         <Text style={{...styles.btnText, color: working ? "white": theme.grey}}>Work</Text>
+         <Text style={{  fontSize:38,
+    fontWeight:"600", color: working ? "white": theme.grey}}>Work</Text>
        </TouchableOpacity>
        <TouchableOpacity onPress={travel}>
-         <Text style={{...styles.btnText, color: !working ? "white": theme.grey}}>Travel</Text>
+         <Text style={{  fontSize:38,
+    fontWeight:"600", color: !working ? "white": theme.grey}}>Travel</Text>
        </TouchableOpacity>
     </View>
       <TextInput 
@@ -96,10 +108,6 @@ const styles = StyleSheet.create({
    marginTop:100,
 
   },
-  btnText:{
-    fontSize:38,
-    fontWeight:"600",
-  },
   input: {
     backgroundColor: "white",
     paddingVertical:8,
@@ -124,3 +132,4 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
 });
+ 
